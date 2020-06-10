0e882de5edbf3ef63e18910163f69df81c7ce304
Charles Short <chucks@redhat.com>
2020-06-10 18:13:17 +0000

    Add pbench-kill-tools
    Signed-off-by: Charles Short <chucks@redhat.com>

--- lib/pbench/cli/commands/tools/__init__.py
+++ lib/pbench/cli/commands/tools/__init__.py
@@ -4,6 +4,7 @@ from . import clear
 from . import list
 from . import start
 from . import stop
+from . import kill
 
 
 @click.group(help="start/stop/kill/register/clear/list pbench tools")
@@ -16,3 +17,4 @@ tools.add_command(clear.clear)
 tools.add_command(list.list)
 tools.add_command(start.start)
 tools.add_command(stop.stop)
+tools.add_command(kill.kill)
